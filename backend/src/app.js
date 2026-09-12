84fe149b67e6f5198d62ef74f9a2c71d2d9b6a5d
Rikesh Kumar <rikeshsinha245@gmail.com>
2026-09-12 16:59:25 +0530

    Add api for the post

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,29 @@
 import express from 'express';
+import multer from 'multer';
+import uploadFile from './services/storage.services.js';
+import postSchema from './models/post.models.js';
 
 const app = express();
 
+app.use(express.json());
+
+const upload = multer({ storage: multer.memoryStorage() });
+
+app.post('/create-post',upload.single('image'), async (req, res) => {
+
+
+        const result = await uploadFile(req.file.buffer);
+         
+        const newPost = new postSchema({
+            image: result.url,
+            caption: req.body.caption,
+        });
+        
+         await newPost.save();
+
+        return res.status(201).json({
+            message: 'Post created successfully',
+        });
+    })
 
 export default app;
