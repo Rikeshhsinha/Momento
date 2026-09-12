a7848539b49113931424c614aaf6d738effe38c0
Rikesh Kumar <rikeshsinha245@gmail.com>
2026-09-12 12:24:06 +0530

    Modify the server

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,21 @@
+import dns from "dns";
+
+dns.setServers([
+    "8.8.8.8",
+    "1.1.1.1"
+]);
+
+
 import app from './src/app.js';
+import connectDB from './src/db/db.js';
+import "dotenv/config";
 
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running on port ${process.env.PORT}`);
+connectDB();
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 }); 
 
 
